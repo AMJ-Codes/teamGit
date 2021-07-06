0238dc16fd6afd94894d8482cfdbaf407b106102
AMJ-Codes <aaronmjeff@gmail.com>
2021-07-06 15:47:07 -0400

    added cards for APIs

--- blue-react/src/components/apps/Home.js
+++ blue-react/src/components/apps/Home.js
@@ -3,7 +3,10 @@ import {
     Link,
     Switch 
 } from 'react-router-dom';
-
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
 import OwApp from './ow-api-app/OpenWeather'
 import TicketMaster from './ticket-api-app/TicketMaster';
 
@@ -16,10 +19,40 @@ const Home = () => {
                 <h2>Check Out the Individual Pages Below: </h2>
                 <hr id="hrHome"/>
                 <ul>
-                    <li><Link to=' ' target='_blank'>NASA by Aaron</Link></li> 
-                    <li><Link to='/weather' target="_blank">Open Weather by Shavonne</Link></li>
-                    <li><Link to='/ticketmaster' target='_blank'>Ticket Master by Max</Link></li> 
-                </ul>
+                <Card>
+        <CardBody>
+          <CardTitle tag="h5">Nasa API</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">By Aaron</CardSubtitle>
+        </CardBody>
+        <img width="100%" src="/assets/318x180.svg" alt="img.svg placeholder" />
+        <CardBody>
+          <CardText>A quick app designed to pull NASA imagery based on your coordinates!</CardText>
+          <li><Link to=' ' target='_blank'>Click here to load the API below!</Link></li>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h5">Weather API</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+        </CardBody>
+        <img width="100%" src="/assets/318x180.svg" alt="img.svg placeholder" />
+        <CardBody>
+          <CardText>Get weather updates in your area with an up to date clock!</CardText>
+          <li><Link to='/weather' target="_blank">Click here to load the app below!</Link></li>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h5">Ticket Master API</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+        </CardBody>
+        <img width="100%" src="/assets/318x180.svg" alt="img.svg placeholder" />
+        <CardBody>
+          <CardText>Find events, attractions and venues based on your location!</CardText>
+          <li><Link to='/ticketmaster' target='_blank'>Click here to load the app below!</Link></li>
+        </CardBody>
+      </Card>
+        </ul>
             </div>
             <div className='mainDiv-route'>
                 <Switch>
