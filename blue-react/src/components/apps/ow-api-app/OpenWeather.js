f1e385069439ce06c33a7a31515a4209371d2e8e
Shavonne Holton <shavonne.holton@gmail.com>
2021-07-06 08:57:29 -0700

    updated weather background

--- blue-react/src/components/apps/ow-api-app/OpenWeather.js
+++ blue-react/src/components/apps/ow-api-app/OpenWeather.js
@@ -1,43 +1,47 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import OwResults from './OwResults';
 import OwClock from './OwClock';
+import GetLocation from '../../../geolocation/GetLocation';
 
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
 const key = '3af00e64fa574fb90f8896a9c6b5f1ba'
 
 const OwApp = () => {
 
-    const [ city, setCity ] = useState('');
     const [ results, setResults ] = useState([]); 
+    const [ latitude, setLatitude ] = useState(null);
+    const [ longitude, setLongitude ] = useState(null);
 
-    const fetchResults = () => {
-        let url = `${baseURL}?q=${city}&appid=${key}`;
+    const fetchWeather = () => {
+        let url = `${baseURL}?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
-        fetch(url)
+        fetch(GetLocation)
             .then(res => res.json())
             .then(data => setResults(data.response.docs))
             .catch(err => console.log(err));
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        fetchResults();
-    }
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     fetchResults();
+    // }
 
     return(
         <div className="main>">
             <div className="mainDiv">
+                <div style={{ background: '#008080' ,color: 'white' }}>
                 <OwClock />
                 <hr />
-                <form onSubmit={(e) => handleSubmit(e)}>
+                {/* <form onSubmit={(e) => handleSubmit(e)}>
                     <span>Enter Your City (require) : </span>
                     <input type="text" name="city" onChange={(e) => setCity(e.target.value)} required />
                     <button className="submit">Submit</button>
-                </form>
+                </form> */}
                 {
                     results.length > 0 ? <OwResults results={results} /> : null
                 }
-            </div>
+                </div>
+                </div>
         </div>
     );
 };
